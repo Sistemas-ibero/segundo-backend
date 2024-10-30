ea0a4c535fb24b638b003636f10c17a2ceb78899
mozta <perezaguirrerafael@gmail.com>
2024-10-30 16:39:14 -0600

    form backend

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+def login(email, password):
+    if email == 'ejemplo@gmail.com' and password == '1234':
+        return True
+    else:
+        print("Usuario o contraseña incorrectos, llegale")
+        return False
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/success' , methods=['GET', 'POST'])
+def success():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    print(email, password)
+
+    resultado = login(email, password)
+    if resultado == False:
+        return render_template('index.html')
+    else:
+        return render_template('success.html')
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
